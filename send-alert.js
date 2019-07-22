f59e04f27324317c4db994e36d5bbcdb97f7d4a4
Luis Rodrigues <luisitaloar@gmail.com>
2019-07-22 13:22:56 -0300

    Envia email para sistema academico e para sistema particular

--- send-alert.js
+++ send-alert.js
@@ -10,8 +10,8 @@ module.exports.sendAlert = (amostra) => {
 	  host     : 'localhost',
 	  user     : 'root',
 	  password : '',
-	  database : 'raiosx',
-	  port: 3307
+	  database : 'raiosx2',
+	  port: 3306
 	});
 
 	const query = 'SELECT u.id_usuario, u.nome, u.email, u.telefone, sa.id_solicitacao, sa.id_usuario, s.identificacao_da_amostra, s.id_solicitacao FROM usuarios as u, solicitacoes_academicas as sa, solicitacoes as s WHERE s.identificacao_da_amostra = "'+amostra+'" AND s.id_solicitacao = sa.id_solicitacao AND sa.id_usuario = u.id_usuario;';
@@ -19,37 +19,69 @@ module.exports.sendAlert = (amostra) => {
 	connectionAc.connect();
 
 	connectionAc.query(query, function (error, results, fields) {
+
 	  if (error) throw error;
-	  if (!results) {
-	  	return ;
+	  if (results.length > 0) {
+		console.log("Entrou no academico");
+		console.log(results);
+		  const result = JSON.parse(JSON.stringify(results));
+
+		  //get number
+		  //numero = result.telefone;
+
+		  //Enviar o email aqui.
+		  sendEmail.sendEmail(result[0]);
 	  }
-
-	  //get number
-	  //numero = results[0].telefone;
-
-	  //Enviar o email aqui.
-	  sendEmail.sendEmail(results);
 	});
 	
 	connectionAc.end();
 
 	//Para o sistema Empresa
-	// ...
+	const connectionEm = mysql.createConnection({
+	  host     : 'localhost',
+	  user     : 'root',
+	  password : '',
+	  database : 'empresa_raiox',
+	  port: 3306
+	});
+
+	const queryEm = 'SELECT u.id, u.email, u.nome, u.telefone, s.id_solicitacao, s.identificacao_da_amostra, sc.id_solicitacao, sc.id_usuario FROM solicitacoes_comerciais as sc, solicitacoes as s, usuarios_comerciais as u WHERE s.identificacao_da_amostra = "'+amostra+'" AND s.id_solicitacao = sc.id_solicitacao AND u.id = sc.id_usuario;';
+	
+	connectionEm.connect();
+
+	connectionEm.query(queryEm, function (error, results, fields) {
+		console.log(error);
+	  if (error) throw error;
+	  if (results.length > 0) {
+		console.log("Entrou no de empresa");
+		console.log(results);
+		  const resultEm = JSON.parse(JSON.stringify(results));
+
+		  //get number
+		  //numero = resultEm.telefone;
+
+		  //Enviar o email aqui.
+		  sendEmail.sendEmail(resultEm[0]);
+	  }
+	});
+	
+	connectionEm.end();
+
 
 	//Enviar via wpp
 	//link https://www.twilio.com/console/sms/whatsapp/sandbox
-	const accountSid = 'ACe66971c91ef1144196c27a9c932c2d0e'; 
-	const authToken = 'ac10ac43b230ede5c9269b374efca35f'; 
-	const client = require('twilio')(accountSid, authToken); 
+	// const accountSid = 'ACe66971c91ef1144196c27a9c932c2d0e'; 
+	// const authToken = 'ac10ac43b230ede5c9269b374efca35f'; 
+	// const client = require('twilio')(accountSid, authToken); 
 	 
-	client.messages 
-	      .create({ 
-	         body: 'Your Twilio code is 1238432', 
-	         from: 'whatsapp:+14155238886',       
-	         to: 'whatsapp:+558597646060' 
-	       }) 
-	      .then(message => console.log(message.sid)) 
-	      .done();
+	// client.messages 
+	//       .create({ 
+	//          body: 'Your Twilio code is 1238432', 
+	//          from: 'whatsapp:+14155238886',       
+	//          to: 'whatsapp:+558597646060' 
+	//        }) 
+	//       .then(message => console.log(message.sid)) 
+	//       .done();
 
 	// const accountSid = 'ACe66971c91ef1144196c27a9c932c2d0e'; 
 	// const authToken = 'ac10ac43b230ede5c9269b374efca35f'; 
--- send-email.js
+++ send-email.js
@@ -1,11 +1,30 @@
 const request = require('request');
+const dateFormat = require('dateformat');
 
-module.exports.sendEmail = (results)=>{
+module.exports.sendEmail = (result)=>{
+	
+	//Assuno e Corpo da mensagem
+	const assunto = '[LRX] Análise Solicitada por '+result.nome;
+	let   corpo = '<p>Olá '+result.nome+',<br> O Sistema de Medida DRX do Laboratório de Raios-X da UFC';
+		  corpo+= 'detectou que sua amostra, identificada como '+result.identificacao_da_amostra+', teve inicío neste exato momento, '+dateFormat(new Date(), "dd/mm/yyyy HH:MM:ss");+'.</p>';
+		  corpo+= '<p> Para acompanhar sua medida, basta acessar o link a baixo. </p>';
+		  corpo+= '<p><a href="http://csd.fisica.ufc.br:8080" target="_blank">Sistema de Medida DRX em Tempo Real</a></p>';
+		  corpo+= '<p>Caso possua alguma dúvida, por favor entre em contato com o Laboratório ';
+		  corpo+= 'por meio do endereço de email lrxufc@gmail.com, ou pelo telefone 85 33669917.</p>';
+		  corpo+= '<p style="text-align:right;">Atenciosamente, <br>Laboratório de Raios-X</p>';
+	
+	const email = result.email;
+
+	//Dados a serem enviados
+	const data = {assunto, corpo, email};
+
+	//dados do post
 	const op = {
-		url:'http://csd.fisica.ufc.br/solicitacoes/send-email.php',
-		form: {key:JSON.stringify(results)}
-	}
+		url:'http://csdint.fisica.ufc.br/solicitacoes/send-email.php',
+		form: {email:JSON.stringify(data)}
+	};
 
+	//Envia o post para mandar o email
 	request.post(op, (err,httpResponse,body) =>{ 
 		console.log(body);
 	});
